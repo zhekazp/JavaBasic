0ea47a8c6b5b325b721deac1db64cbc9d61eb1fb
zhekazp <zhekazp21@gmail.com>
2024-01-17 16:56:54 +0200

    Initial commit

---
+++ HomeWork/HomeWork2/HomeWork2_1.java
@@ -0,0 +1,28 @@
+package homeWork;
+public class HomeWork2_1 {
+    public static void main(String[] args) {
+        // char: G int: 89 byte: 4 short: 56 float: 4.7333436 double: 4.355453532 long: 12121
+        char letter='G';
+        int digit=89;
+        byte digSmall=4;
+        short digitMiddl=56;
+        float digPoint=4.7333436f;
+        double digPointBig=4.355453532;
+        long digbig=12121;
+
+        System.out.println();
+        System.out.println("This is char- "+letter);
+        System.out.println();
+        System.out.println("This is int- "+digit);
+        System.out.println();
+        System.out.println("This is byte- "+digSmall);
+        System.out.println();
+        System.out.println("This is short- "+digitMiddl);
+        System.out.println();
+        System.out.println("This is float- "+digPoint);
+        System.out.println();
+        System.out.println("This is double- "+digPointBig);
+        System.out.println();
+        System.out.println("This is long- "+digbig);
+    }
+}
---
+++ HomeWork/HomeWork2/HomeWork2_2.java
@@ -0,0 +1,11 @@
+package homeWork;
+public class HomeWork2_2 {
+    public static void main(String[] args) {
+        int digit=456;
+        int result1=digit/100;
+        int result2=2+3;
+        int result3=digit-450;
+        System.out.println();
+        System.out.println("Число :"+digit+" -> "+result1+", "+result2+", "+result3);
+    }
+}
