091f46130c71b29590712d8080464439d743860d
zhekazp <zhekazp21@gmail.com>
2024-01-18 16:01:03 +0200

    Initial commit

--- HomeWork/HomeWork2/HomeWork2_2.java
+++ HomeWork/HomeWork2/HomeWork2_2.java
@@ -1,10 +1,11 @@
 package homeWork;
+
 public class HomeWork2_2 {
     public static void main(String[] args) {
-        int digit=456;
+        int digit=476;
         int result1=digit/100;
-        int result2=digit-451;
-        int result3=digit-450;
+        int result2=result1+3;
+        int result3=result2-1;
         System.out.println();
         System.out.println("Число :"+digit+" -> "+result1+", "+result2+", "+result3);
     }
