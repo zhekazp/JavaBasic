58fbcf3859455fd942d720ba7c77c828ab6d9f72
zhekazp <zhekazp21@gmail.com>
2024-02-01 19:46:53 +0200

    Initial commit

---
+++ homeWork/homeWork13/HomeWork13_2.java
@@ -0,0 +1,17 @@
+package homeWork13;
+
+import homeWork13.classes.ManagerHW2;
+
+public class HomeWork13_2 {
+    public static void main(String[] args) {
+        ManagerHW2 a=new ManagerHW2();
+
+        System.out.println(a.printArray());
+
+        if (a.testIncrease()){
+            System.out.println("The array is increasing");
+        }else {
+            System.out.println("The array is not increasing");
+        }
+    }
+}
